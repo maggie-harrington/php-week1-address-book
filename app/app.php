3a23f4e2b70269764e0da972586ec1a2fd6551cc
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-10 08:57:14 -0800

    create ['list_of_contacts'], add create_contacts and delete_contacts basic page and route structure

--- app/app.php
+++ app/app.php
@@ -5,6 +5,9 @@
     require_once __DIR__."/../src/Contact.php";
 
     session_start();
+    if (empty($_SESSION['list_of_contacts'])) {
+        $_SESSION['list_of_contacts'] = array();
+    }
 
     $app = new Silex\Application();
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -15,6 +18,14 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('home.html.twig');
+    });
+
+    $app->post("/create_contact", function() use ($app) {
+        return $app['twig']->render('create_contact.html.twig');
+    });
+
+    $app->post("/delete_contacts", function() use ($app) {
+        return $app['twig']->render('delete_contacts.html.twig');
     });
 
     return $app;
